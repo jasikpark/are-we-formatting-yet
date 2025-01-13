be5a78216f923d71f89abf1e1dfe6a29ce6aa234
Caleb Jasik <calebjasik@jasik.xyz>
2025-01-12 22:15:58 -0600

    Make the linter happy

--- src/components/counter.tsx
+++ src/components/counter.tsx
@@ -11,6 +11,7 @@ export const Counter = () => {
     <section className="-mx-4 mt-4 rounded border border-dashed border-blue-400 p-4">
       <div>Count: {count}</div>
       <button
+        type="button"
         onClick={handleIncrement}
         className="rounded-sm bg-black px-2 py-0.5 text-sm text-white"
       >
