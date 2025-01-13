62fcb359dc4765a941dc3b15feb4f72dc5c9b4ac
Caleb Jasik <calebjasik@jasik.xyz>
2025-01-12 22:17:10 -0600

    Don't format pages.gen.ts

--- src/pages.gen.ts
+++ src/pages.gen.ts
@@ -3,11 +3,9 @@ import type { GetConfigResponse, PathsForPages } from "waku/router";
 import type { getConfig as About_getConfig } from "./pages/about";
 import type { getConfig as Index_getConfig } from "./pages/index";
 
-type Page = {
-  DO_NOT_USE_pages:
-    | ({ path: "/about" } & GetConfigResponse<typeof About_getConfig>)
-    | ({ path: "/" } & GetConfigResponse<typeof Index_getConfig>);
-};
+type Page =
+  | ({ path: "/about" } & GetConfigResponse<typeof About_getConfig>)
+  | ({ path: "/" } & GetConfigResponse<typeof Index_getConfig>);
 
 declare module "waku/router" {
   interface RouteConfig {
