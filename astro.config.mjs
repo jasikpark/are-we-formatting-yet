1dde34515f9b786c561f4ae79eb25ba094e0ebbf
Caleb Jasik <calebjasik@jasik.xyz>
2026-02-10 21:39:12 -0600

    biome fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: 'https://areweformattingyet.com',
+	site: "https://areweformattingyet.com",
 });
